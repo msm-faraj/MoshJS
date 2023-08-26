e8151e6584f1d07cbcb169f49b6d60d01d747e1c
msm-faraj <meysamfarajzade@gmail.com>
2023-08-26 15:22:09 +0200

    showPrime function is added

---
+++ JS-Bsics/showPrime.js
@@ -0,0 +1,14 @@
+function showPrime(limit) {
+  for (let i = 2; i <= limit; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) console.log(i);
+  }
+}
+
+showPrime(1000);
