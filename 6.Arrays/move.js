ed91ce5c16a2227e298721c9b7c56d9d931c5737
msm-faraj <farajzademeysam@gmail.com>
2023-09-07 18:05:07 +0200

    move function added

---
+++ 6.Arrays/move.js
@@ -0,0 +1,11 @@
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.error("invalid offset.");
+    return;
+  }
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(index + offset, 0, element);
+  return output;
+}
